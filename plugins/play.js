deae514a2cbafaf966d3e9542237c771ced6ff22
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2021-12-29 11:51:45 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 *Ukuran File Audio:* ${filesizeF}
 *Ukuran File Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
-`.trim(), '© A N D Y - B O T Z', 'Audio Nya Bang', `.yta ${vid.url}`, 'Video Nya Bang', `.yt ${vid.url}`)
+`.trim(), '© KRIZSER', 'Audio Nya Bang', `.yta ${vid.url}`, 'Video Nya Bang', `.yt ${vid.url}`)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['premium']
